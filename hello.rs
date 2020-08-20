dfc05c922549f3ef2f884e806b181dcbe3e4453e
saadhika <saadhika@adobe.com>
2020-08-20 17:47:22 -0400

    added dummy code

---
+++ hello.rs
@@ -0,0 +1,22 @@
+
+fn main() {
+
+    let rows:i16 = 4
+    let cols:i16 = 4
+    let generations:i16 = 4
+    life(rows, cols, generations)
+}
+
+fn life(rows:i16, cols:i16, generations:i16){
+    //
+    live()
+}
+
+fn live()
+{
+
+}
+
+
+// random generator function
+// 
